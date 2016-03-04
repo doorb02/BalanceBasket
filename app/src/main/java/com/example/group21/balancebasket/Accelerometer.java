acbaef183aae316ec7f9e54a1428e18676bf0cf0
cornebrink <cornebrink@hotmail.nl>
2016-03-04 15:27:39 +0100

    vs 1

--- app/src/main/java/com/example/group21/balancebasket/Accelerometer.java
+++ app/src/main/java/com/example/group21/balancebasket/Accelerometer.java
@@ -2,13 +2,28 @@ package com.example.group21.balancebasket;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class Accelerometer extends AppCompatActivity {
+    public TextView Xas;
+    public TextView Yas;
+    public TextView Zas;
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_accelerometer);
+
+        Xas = (TextView) findViewById(R.id.x_as);
+        Yas = (TextView) findViewById(R.id.y_as);
+        Zas = (TextView) findViewById(R.id.z_as);
+
+
     }
 }
+
 
