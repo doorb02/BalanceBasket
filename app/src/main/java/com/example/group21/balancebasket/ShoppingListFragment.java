b20e535718cea3aeea9626724f756497f6d88b7d
tqmallon <thomas_oo@hotmail.com>
2016-03-30 13:03:12 +0200

    Buttons en EditText toegevoegd

--- app/src/main/java/com/example/group21/balancebasket/ShoppingListFragment.java
+++ app/src/main/java/com/example/group21/balancebasket/ShoppingListFragment.java
@@ -11,7 +11,10 @@ import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -28,6 +31,10 @@ public class ShoppingListFragment extends Fragment {
     private Cursor cursor;
     private ListDataAdapter listDataAdapter;
     private DataProvider dataProvider;
+    private EditText name;
+    private EditText price;
+    private Button AddButton;
+    private Button RemoveButton;
 
     public ShoppingListFragment() {
         // Required empty public constructor
@@ -38,10 +45,24 @@ public class ShoppingListFragment extends Fragment {
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_shopping_list, container, false);
         listView = (ListView) view.findViewById(R.id.ListView);
+        name = (EditText) view.findViewById(R.id.NameEditText);
+        price = (EditText) view.findViewById(R.id.PriceEditText);
+        AddButton = (Button) view.findViewById(R.id.AddButton);
+        AddButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getActivity(), "AddButtonClicked", Toast.LENGTH_LONG).show();
+            }
+        });
+        RemoveButton = (Button) view.findViewById(R.id.RemoveButton);
         listDataAdapter = new ListDataAdapter(this.getContext(), R.layout.row_layout);
         provideData();
         listView.setAdapter(listDataAdapter);
         return view;
+
+
+
+
     }
 
     // get products from the database and add them to the shopping list
@@ -78,4 +99,6 @@ public class ShoppingListFragment extends Fragment {
     public interface OnFragmentInteractionListener {
         void onFragmentInteraction(Uri uri);
     }
+
+
 }
